b126b0870b147ded02426b794f57a17b0089128b
Robert-Hein Hooijmans <rh.hooijmans@gmail.com>
2017-05-03 22:56:55 +0200

    Add BounceStyle.

--- BouncyLayout/Classes/BouncyLayout.swift
+++ BouncyLayout/Classes/BouncyLayout.swift
@@ -2,18 +2,53 @@ import UIKit
 
 public class BouncyLayout: UICollectionViewFlowLayout {
     
+    public enum BounceStyle {
+        case subtle
+        case regular
+        case prominent
+        
+        var damping: CGFloat {
+            switch self {
+            case .subtle: return 1
+            case .regular: return 0.75
+            case .prominent: return 0.5
+            }
+        }
+        
+        var frequency: CGFloat {
+            switch self {
+            case .subtle: return 2
+            case .regular: return 1.5
+            case .prominent: return 1
+            }
+        }
+    }
+    
+    private var damping: CGFloat = BounceStyle.regular.damping
+    private var frequency: CGFloat = BounceStyle.regular.frequency
+    
+    public convenience init(style: BounceStyle) {
+        self.init()
+        
+        damping = style.damping
+        frequency = style.frequency
+    }
+    
+    public convenience init(damping: CGFloat, frequency: CGFloat) {
+        self.init()
+        
+        self.damping = damping
+        self.frequency = frequency
+    }
+    
     private lazy var animator: UIDynamicAnimator = UIDynamicAnimator(collectionViewLayout: self)
     
     public override func prepare() {
         super.prepare()
         guard let view = collectionView, let attributes = super.layoutAttributesForElements(in: view.bounds)?.flatMap({ $0.copy() as? UICollectionViewLayoutAttributes }) else { return }
         
-        oldBehaviors(for: attributes).forEach(animator.removeBehavior(_:))
-        newBehaviors(for: attributes).forEach {
-            $0.damping = 0.7
-            $0.frequency = 1.5
-            animator.addBehavior($0)
-        }
+        oldBehaviors(for: attributes).forEach { animator.removeBehavior($0) }
+        newBehaviors(for: attributes).forEach { animator.addBehavior($0, damping, frequency) }
     }
     
     private func oldBehaviors(for attributes: [UICollectionViewLayoutAttributes]) -> [UIAttachmentBehavior] {
@@ -53,5 +88,14 @@ public class BouncyLayout: UICollectionViewFlowLayout {
         let resistance = CGVector(dx: fabs(view.panGestureRecognizer.location(in: view).x - behavior.anchorPoint.x) / 1000, dy: fabs(view.panGestureRecognizer.location(in: view).y - behavior.anchorPoint.y) / 1000)
         item.center.y += delta.dy < 0 ? max(delta.dy, delta.dy * resistance.dy) : min(delta.dy, delta.dy * resistance.dy)
         item.center.x += delta.dx < 0 ? max(delta.dx, delta.dx * resistance.dx) : min(delta.dx, delta.dx * resistance.dx)
+    }
+}
+
+extension UIDynamicAnimator {
+    
+    open func addBehavior(_ behavior: UIAttachmentBehavior, _ damping: CGFloat, _ frequency: CGFloat) {
+        behavior.damping = damping
+        behavior.frequency = frequency
+        addBehavior(behavior)
     }
 }
